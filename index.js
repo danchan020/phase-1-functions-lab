f8144196aad47c7dd5febe2341ddd7a12ae7205e
Daniel <danchan020@gmail.com>
2022-04-26 02:23:54 -0700

    commit tree 70a7f37f3b9ab8fe519a5eb2dcea9a0662acff21

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if (block < 42){
+            return (42 - block);
+        }
+    else {
+        return (block -42);
+    }
+    }
+    function distanceFromHqInFeet (someValue) {
+        return distanceFromHqInBlocks(someValue)*264;
+    }
+
+    function distanceTravelledInFeet(a,b){
+        if (b>a){
+          return((b-a)*264);
+        }
+        else
+          return((a-b)*264);
+    }
+
+    function calculatesFarePrice (a,b){
+        let distance = distanceTravelledInFeet(a,b);
+        if (distance <= 400){return 0;}
+        if (distance > 400 && distance < 2000){return (distance - 400) * 0.02;}
+        else if (distance >= 2000 && distance <= 2500){return 25;}
+        else if (distance > 2500){return "cannot travel that far";}
+    }
